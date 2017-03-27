dba89e5bc6a2e4cd70dfc9d6155dddaea43c587c
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-27 18:53:31 +0300

    Added WeatherPreviewFragment class with layout file

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/ui/WeatherPreviewFragment.java
@@ -0,0 +1,10 @@
+package tsekhmeistruk.whatistheweather.ui;
+
+import android.app.Fragment;
+
+/**
+ * Created by Roman Tsekhmeistruk on 27.03.2017.
+ */
+
+public class WeatherPreviewFragment extends Fragment {
+}
