82fd14fa8fc5775b206a4d6326dd7fa56d9ae469
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-27 19:06:51 +0300

    Implemented onCreate(...) and onCreateView(...) methods of fragment

--- app/src/main/java/tsekhmeistruk/whatistheweather/ui/WeatherPreviewFragment.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/ui/WeatherPreviewFragment.java
@@ -1,10 +1,30 @@
 package tsekhmeistruk.whatistheweather.ui;
 
-import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import tsekhmeistruk.whatistheweather.R;
 
 /**
  * Created by Roman Tsekhmeistruk on 27.03.2017.
  */
 
 public class WeatherPreviewFragment extends Fragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.weather_preview_fragment, container, false);
+        return view;
+    }
+
 }
