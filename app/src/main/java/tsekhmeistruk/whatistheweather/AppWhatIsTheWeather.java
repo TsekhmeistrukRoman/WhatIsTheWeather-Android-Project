a40904a9cd26b450bf247860e3d8c9ec9d929cff
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-27 18:39:06 +0300

    Added application class

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/AppWhatIsTheWeather.java
@@ -0,0 +1,10 @@
+package tsekhmeistruk.whatistheweather;
+
+import android.support.multidex.MultiDexApplication;
+
+/**
+ * Created by Roman Tsekhmeistruk on 27.03.2017.
+ */
+
+public class AppWhatIsTheWeather extends MultiDexApplication {
+}
