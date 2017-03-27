95d7902500577ccb017b597dc7b844d0295ac8f6
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-27 18:36:35 +0300

    Main activity class was moved to "activities" package

--- app/src/main/java/tsekhmeistruk/whatistheweather/activities/MainActivity.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package tsekhmeistruk.whatistheweather;
+package tsekhmeistruk.whatistheweather.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import tsekhmeistruk.whatistheweather.R;
 
 public class MainActivity extends AppCompatActivity {
 
