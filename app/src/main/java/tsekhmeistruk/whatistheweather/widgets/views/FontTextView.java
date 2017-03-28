a30e1d39be48db25e331919144626e853a7df743
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-28 16:47:56 +0300

    Added font text view

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/widgets/views/FontTextView.java
@@ -0,0 +1,21 @@
+package tsekhmeistruk.whatistheweather.widgets.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by Roman Tsekhmeistruk on 28.03.2017.
+ */
+
+public class FontTextView extends TextView {
+
+    public FontTextView(Context context) {
+        this(context, null);
+    }
+
+    public FontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+}
