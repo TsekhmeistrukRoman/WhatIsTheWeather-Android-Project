42a414dea35709ba0c3d3fa17e0502967a3c9762
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-28 14:58:56 +0300

    Added BaseView class

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/views/BaseView.java
@@ -0,0 +1,13 @@
+package tsekhmeistruk.whatistheweather.views;
+
+import android.content.Context;
+
+/**
+ * Created by Roman Tsekhmeistruk on 28.03.2017.
+ */
+
+public interface BaseView {
+
+    Context getContext();
+
+}
