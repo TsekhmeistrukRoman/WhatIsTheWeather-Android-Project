4de84c07dbd54e60153db3f932c17e00079b8e30
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-28 16:59:35 +0300

    Added string resource for displaying toast if request execution has failed

--- app/src/main/java/tsekhmeistruk/whatistheweather/utils/RxErrorAction.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/utils/RxErrorAction.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.widget.Toast;
 
 import rx.functions.Action1;
+import tsekhmeistruk.whatistheweather.R;
 
 /**
  * Created by Roman Tsekhmeistruk on 27.02.2017.
@@ -11,17 +12,18 @@ import rx.functions.Action1;
 
 public class RxErrorAction implements Action1<Throwable> {
 
-    private final Context mContext;
+    private final Context context;
 
     public RxErrorAction(Context context) {
-        mContext = context;
+        this.context = context;
     }
 
     @Override
     public void call(Throwable throwable) {
         throwable.printStackTrace();
 
-        Toast.makeText(mContext, "Request execution has failed", Toast.LENGTH_SHORT).show();
+        Toast.makeText(context, context.getString(R.string.request_execution_has_failed),
+                Toast.LENGTH_SHORT).show();
     }
 
 }
