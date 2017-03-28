f3b3eabdb1e02163a4a66be61e9c95d42ef7b2ff
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-28 14:02:23 +0300

    Realized scopes

--- app/src/main/java/tsekhmeistruk/whatistheweather/di/scopes/Scope.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/di/scopes/Scope.java
@@ -1,8 +1,15 @@
 package tsekhmeistruk.whatistheweather.di.scopes;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 /**
  * Created by Roman Tsekhmeistruk on 28.03.2017.
+ * Scope annotation for different scopes.
  */
 
-public interface Scope {
+@javax.inject.Scope
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Scope {
+    String value() default "";
 }
--- app/src/main/java/tsekhmeistruk/whatistheweather/di/scopes/Scopes.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/di/scopes/Scopes.java
@@ -2,7 +2,19 @@ package tsekhmeistruk.whatistheweather.di.scopes;
 
 /**
  * Created by Roman Tsekhmeistruk on 28.03.2017.
+ * Constants variables for modules and providers.
  */
 
 public class Scopes {
+
+    /** Lifecycle scope annotation constants. */
+    public static final String ACTIVITY  = "activity";
+    public static final String SERVICE   = "service";
+    public static final String FRAGMENT  = "fragment";
+    public static final String VIEW      = "view";
+
+    private Scopes() {
+        throw new AssertionError("Unable to instantiate");
+    }
+
 }
