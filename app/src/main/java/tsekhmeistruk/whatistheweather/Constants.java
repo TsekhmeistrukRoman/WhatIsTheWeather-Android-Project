26b489929218a6a1d6aa4e1b28041a5935c94f03
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-28 16:38:19 +0300

    Added google places api key constant

--- app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
@@ -9,4 +9,6 @@ public class Constants {
     private static final String GOOGLE_MAP_SERVER = "https://maps.googleapis.com/maps";
     public static final String GOOGLE_MAP_API = GOOGLE_MAP_SERVER + "/api/";
 
+    public static final String GOOGLE_PLACES_API_KEY = "AIzaSyA5zBaZbf5i9dOA-UgpuuarldxUZ-Ddzp0";
+
 }
