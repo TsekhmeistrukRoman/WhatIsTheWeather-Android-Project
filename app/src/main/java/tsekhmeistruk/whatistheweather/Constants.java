570b353ed44cbfefcf35a5e42e3bfe207e6e8b73
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-29 10:03:42 +0300

    Added Open Weather Api Key

--- app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
@@ -10,5 +10,6 @@ public class Constants {
     public static final String GOOGLE_MAP_API = GOOGLE_MAP_SERVER + "/api/";
 
     public static final String GOOGLE_PLACES_API_KEY = "AIzaSyA5zBaZbf5i9dOA-UgpuuarldxUZ-Ddzp0";
+    public static final String OPEN_WEATHER_API_KEY = "d634bba455936939a1ac5189b96d1830";
 
 }
