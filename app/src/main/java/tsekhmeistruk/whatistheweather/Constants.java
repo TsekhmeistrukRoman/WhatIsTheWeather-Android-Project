b9e792d45f07e4e0a32b363c6b3d814135be9f4a
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-30 16:18:35 +0300

    Removed unused google places API

--- app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/Constants.java
@@ -8,8 +8,7 @@ public class Constants {
 
     private static final String GOOGLE_MAP_SERVER = "https://maps.googleapis.com/maps";
     public static final String GOOGLE_MAP_API = GOOGLE_MAP_SERVER + "/api/";
-
-    public static final String GOOGLE_PLACES_API_KEY = "AIzaSyA5zBaZbf5i9dOA-UgpuuarldxUZ-Ddzp0";
+    
     public static final String OPEN_WEATHER_API_KEY = "d634bba455936939a1ac5189b96d1830";
 
 }
--- app/src/main/java/tsekhmeistruk/whatistheweather/api/GooglePlacesApi.java
+++
@@ -1,8 +0,0 @@
-package tsekhmeistruk.whatistheweather.api;
-
-/**
- * Created by Roman Tsekhmeistruk on 28.03.2017.
- */
-
-public interface GooglePlacesApi {
-}
