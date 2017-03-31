e3bb248f8bed590f25f59d22b219637679d1cb2d
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-31 12:58:06 +0300

    Added method for app component getting

--- app/src/main/java/tsekhmeistruk/whatistheweather/AppWhatIsTheWeather.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/AppWhatIsTheWeather.java
@@ -2,9 +2,30 @@ package tsekhmeistruk.whatistheweather;
 
 import android.support.multidex.MultiDexApplication;
 
+import tsekhmeistruk.whatistheweather.di.component.AppComponent;
+import tsekhmeistruk.whatistheweather.di.component.DaggerAppComponent;
+import tsekhmeistruk.whatistheweather.di.module.ApiModule;
+import tsekhmeistruk.whatistheweather.di.module.AppModule;
+
 /**
  * Created by Roman Tsekhmeistruk on 27.03.2017.
  */
 
 public class AppWhatIsTheWeather extends MultiDexApplication {
+
+    private AppComponent appComponent;
+
+    public AppWhatIsTheWeather() {
+        super();
+
+        appComponent = DaggerAppComponent.builder()
+                .apiModule(new ApiModule())
+                .appModule(new AppModule(this))
+                .build();
+    }
+
+    public AppComponent appComponent() {
+        return appComponent;
+    }
+
 }
