85cdc65c668053b54fb4d1edf8bdc233771d8303
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-31 12:21:30 +0300

    Added WeatherForecast presenter

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/presenters/WeatherForecastPresenter.java
@@ -0,0 +1,43 @@
+package tsekhmeistruk.whatistheweather.presenters;
+
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
+import tsekhmeistruk.whatistheweather.models.remote.IWeatherForecastDataSource;
+import tsekhmeistruk.whatistheweather.utils.RxErrorAction;
+import tsekhmeistruk.whatistheweather.utils.RxRetryWithDelay;
+import tsekhmeistruk.whatistheweather.views.WeatherForecastView;
+
+/**
+ * Created by Roman Tsekhmeistruk on 31.03.2017.
+ */
+
+public class WeatherForecastPresenter extends BasePresenter<WeatherForecastView> {
+
+    private final IWeatherForecastDataSource weatherForecastDataSource;
+
+    public WeatherForecastPresenter(IWeatherForecastDataSource weatherForecastDataSource) {
+        this.weatherForecastDataSource = weatherForecastDataSource;
+    }
+
+    public void loadWeatherForecast(double latitude,
+                                    double longitude,
+                                    int daysNumber,
+                                    String mode,
+                                    String appId,
+                                    String units) {
+        WeatherForecastView view = getView();
+        subscribe(
+                weatherForecastDataSource.getWeatherForecast(latitude, longitude,
+                        daysNumber, mode,
+                        appId, units)
+                        .retryWhen(new RxRetryWithDelay())
+                        .map(response -> response)
+                        .subscribeOn(Schedulers.io())
+                        .observeOn(AndroidSchedulers.mainThread())
+                        .subscribe(view::showWeatherForecast,
+                                new RxErrorAction(getView().getContext())
+                        )
+        );
+    }
+
+}
