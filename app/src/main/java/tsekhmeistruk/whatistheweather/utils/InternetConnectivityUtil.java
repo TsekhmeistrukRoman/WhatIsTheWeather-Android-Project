9455c2256490e22b3775f37358a7fd39fc8dbdfd
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-03-31 18:04:40 +0300

    ConnectivityUtil class was renamed

--- app/src/main/java/tsekhmeistruk/whatistheweather/utils/InternetConnectivityUtil.java
+++ app/src/main/java/tsekhmeistruk/whatistheweather/utils/InternetConnectivityUtil.java
@@ -8,25 +8,25 @@ import android.net.NetworkInfo;
  * Created by Roman Tsekhmeistruk on 31.03.2017.
  */
 
-public class ConnectivityUtil {
+public class InternetConnectivityUtil {
 
     private static NetworkInfo getNetworkInfo(Context context) {
         ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         return cm.getActiveNetworkInfo();
     }
-    
+
     public static boolean isConnected(Context context) {
-        NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
+        NetworkInfo info = InternetConnectivityUtil.getNetworkInfo(context);
         return (info != null && info.isConnected());
     }
 
     public static boolean isConnectedWifi(Context context) {
-        NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
+        NetworkInfo info = InternetConnectivityUtil.getNetworkInfo(context);
         return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_WIFI);
     }
 
     public static boolean isConnectedMobile(Context context) {
-        NetworkInfo info = ConnectivityUtil.getNetworkInfo(context);
+        NetworkInfo info = InternetConnectivityUtil.getNetworkInfo(context);
         return (info != null && info.isConnected() && info.getType() == ConnectivityManager.TYPE_MOBILE);
     }
 
