444417600d350b97d9614dbd081479d89e59fa05
Roman Tsekhmeistruk <roman.tsekhmeistruk@gmail.com>
2017-04-04 19:17:59 +0300

    Added LocationUtil class for checking if location services is enabled

---
+++ app/src/main/java/tsekhmeistruk/whatistheweather/utils/LocationUtil.java
@@ -0,0 +1,19 @@
+package tsekhmeistruk.whatistheweather.utils;
+
+import android.content.Context;
+import android.location.LocationManager;
+
+/**
+ * Created by Roman Tsekhmeistruk on 31.03.2017.
+ */
+
+public class LocationUtil {
+
+    public static boolean isEnabled(Context context) {
+        LocationManager locationManager
+                = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+        return ((locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER))
+                && (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)));
+    }
+
+}
